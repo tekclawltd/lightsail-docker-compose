df99bfa9ecf4bc2f7c42c92c5f3d08c97b8d4456
tekclawltd <102192393+tekclawltd@users.noreply.github.com>
2022-08-28 12:07:30 +0100

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -11,7 +11,7 @@ usermod -aG docker ubuntu
 
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 mkdir -p /srv
 chown ubuntu -R /srv
